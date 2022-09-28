9685f9d20a3816c801e0deff35b9a0d72f95c336
DataIntellSystLab <59449016+DataIntellSystLab@users.noreply.github.com>
2022-09-28 17:10:43 -0400

    Add files via upload

--- DynaPhyloPart.R
+++ DynaPhyloPart.R
@@ -5,14 +5,23 @@ library(ape)
 library(castor)
 library(phytools)
 
-setwd("C:/Users/m.prosperi/Downloads/PhyloPart_v2.1")
-tree=read.tree("nextstrain_ncov_open_global_all-time_tree.nwk")
+################################################################
+##### SET YOUR HOME DIRECTORY AND READ THE TREE HERE
 
-#tree=rtree(333)
+setwd("C:/Users/m.prosperi/Downloads/bibm22-phylopart")
+treefilename="nextstrain_ncov_open_global_all-time_tree.nwk"
+tree=read.tree(treefilename)
 
-minpercthresh=0.25
-minnumclus=3
-dist_limit=25000
+#tree=rtree(333)  #RANDOM TREE IF YOU JUST WANNA PLAY
+
+################################################################
+##### SET YOUR PARAMETERS HERE
+
+minpercthresh=0.33  ## PERCENTILE THRESHOLD
+minnumclus=3        ## MINIMUM CLUSTER SIZE
+dist_limit=25000    ## LIMIT ON PAIRWISE DISTANCES' CALCULATION
+
+################################################################
 
 nallnodes=Ntip(tree)+Nnode(tree)
 if (nallnodes*(nallnodes-1)/2<dist_limit) {dist_limit=nallnodes*(nallnodes-1)/2}
@@ -32,7 +41,7 @@ for (i in samplenodes1)
 			dab=get_pairwise_distances(tree, i, j)
 			dist_mat=rbind(dist_mat,c(i,j,dab))
 			lim=lim+1
-			if (lim%%6666==0) {cat(round(100*lim/dist_limit,1),"%\n",sep="")}
+			if (lim%%1000==0) {cat(round(100*lim/dist_limit,1),"%\n",sep="")}
 			if (lim==dist_limit) break;
 		}
 	}
@@ -40,7 +49,6 @@ for (i in samplenodes1)
 }
 dist_mat=na.omit(dist_mat)
 thres=quantile(dist_mat[,3],minpercthresh)
-
 
 innernodes=unique(tree$edge[,1])
 clusts=list()
@@ -54,22 +62,49 @@ for (i in innernodes)
 		desc=getDescendants(tree,i);
 		clu=i
 		dis=NULL
-		for (k in desc)
+		while (length(desc)>0)
 		{
+			k=desc[1]
+			desc=desc[-1]
 			ds=get_pairwise_distances(tree,i,k)
-			if (!is.na(ds)) {if (ds<=thres) {clu=c(clu,k)}}
+			if (!is.na(ds)) 
+			{
+				if (ds<=thres) {clu=c(clu,k)}
+				else
+				{
+					descdesc=getDescendants(tree,k);
+					desc=setdiff(desc,descdesc)
+				}
+			}
 		}
 		c=c+1
 		clusts[[c]]=clu
-		if (i%%10==0) cat("node",i,"\n")
+		if (i%%10==0) cat(round(100*i/max(innernodes),1),"%\n")
 	}
 }
+nclus=length(clusts)
+for (i in 1:Ntip(tree))
+{
+	cfound=F
+	for (j in 1:length(clusts)) {if (i %in% clusts[[j]]) {cfound=T; break;}}
+	if (!cfound) {clusts[[nclus+1]]=i; nclus=nclus+1;}
+}
 
-nclus=0
-for (i in 1:length(clusts)) {if (length(clusts[[i]])>=minnumclus) nclus=nclus+1}
-
-clusts
-nclus
+clusttab=NULL
+for (i in 1:length(clusts)) 
+{
+	cluster_id=paste("unclustered",i,sep="_")
+	if (length(clusts[[i]])>=minnumclus) {cluster_id=paste("clustered",i,sep="_")}
+	for (j in 1:length(clusts[[i]]))
+	{
+		leaf_id=NA
+		isLeaf=clusts[[i]][j]<=Ntip(tree)
+		if (isLeaf) {leaf_id=tree$tip.label[clusts[[i]][j]]}
+		clusttab=rbind(clusttab,c(clusts[[i]][j],cluster_id,leaf_id))
+	}
+}
+clusttab=as.data.frame(clusttab)
+names(clusttab)=c("node_id","cluster_id","leaf_id")
 
 palett=rainbow(length(clusts)); #rainbow; topo.colors; heat.colors; cm.colors; terrain.colors; colors()
 palett=sample(palett)
@@ -90,6 +125,13 @@ for (i in 1:length(tipcols))
 	for (j in 1:length(clusts)) {if (length(clusts[[j]])>=minnumclus && i %in% clusts[[j]]) {tipcols[i]=palett[j];break;}}
 }
 
+
+################################################################
+##### PLOTTING TREE AND PRINTING CLUSTERS' TABLE
+
 plot(tree,show.tip.label=F,type="f",edge.color=edgecols)
 #tiplabels(frame="none",col=tipcols,cex=0.8)
 #nodelabels(frame="none",col=nodecols,cex=0.8)
+
+write.csv(clusttab,file=paste(treefilename,".csv",sep=""),row.names=F,quote=F)
+
